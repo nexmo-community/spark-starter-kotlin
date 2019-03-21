2d59b3d7066a3f6614d6c29190cc015afb88499b
Steve Crow <steve.crow@vonage.com>
2019-03-21 13:57:41 -0400

    Check for a port defined in the environment instead of hardcoding the value.

--- src/main/kotlin/com/nexmo/starter/App.kt
+++ src/main/kotlin/com/nexmo/starter/App.kt
@@ -1,14 +1,23 @@
 package com.nexmo.starter
 
-import spark.kotlin.*
+import spark.kotlin.get
+import spark.kotlin.port
+import spark.kotlin.post
 
 class App
 
+const val PORT = 3000
+
 fun main() {
     // Set the listening port to 3000
-    port(3000)
+    port(getPort())
 
     // Register the routes
     get("/example") { "Hello world!" }
     post("/example") { "Hello world!" }
+}
+
+fun getPort(): Int {
+    val port = System.getenv("PORT")
+    return if (port != null) port.toInt() else PORT
 }
